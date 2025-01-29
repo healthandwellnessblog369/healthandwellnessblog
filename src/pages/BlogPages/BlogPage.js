4fb08f4f639cb61eff6ccc8b320ff50a8cd9ff34
Mathew <doakmath@gmail.com>
2025-01-29 16:41:48 -0600

    image experiment

--- src/pages/BlogPages/BlogPage.js
+++ src/pages/BlogPages/BlogPage.js
@@ -82,7 +82,19 @@ const BlogPage = () => {
           )}
           {section.adSpace && (
             <div className="adspace-mobile">
-              <p>Ad Space</p>
+              <div>
+                <a
+                  href="https://www.amazon.com/dp/B07ZS3T3PM?ref=t_ac_view_request_product_image&campaignId=amzn1.campaign.2NN8UGEPQAGAP&linkCode=tr1&tag=healthand0f57-20&linkId=amzn1.campaign.2NN8UGEPQAGAP_1738187052541"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="https://m.media-amazon.com/images/I/71YO-q1eS-L.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+                    alt="Amazon Product"
+                    style={{ width: "100%", maxWidth: "300px" }}
+                  />
+                </a>
+              </div>
             </div>
           )}
         </div>
